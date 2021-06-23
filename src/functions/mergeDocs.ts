5808ae656e54cffa5ca7975603483e67ce0a1abe
Abdulbaki Dursun <eggsydev@gmail.com>
2021-06-24 01:54:51 +0300

    feat: merge and sort two arrays function

---
+++ src/functions/mergeDocs.ts
@@ -0,0 +1,22 @@
+import moment from "moment";
+
+/* Import types */
+import type { Song } from "../types/Song";
+
+/**
+ * Merge and sort docs.
+ * @param firstArray Song[]
+ * @param secondArray Song[]
+ * @returns Song[]
+ */
+export default function mergeDocs(
+  firstArray: Song[],
+  secondArray: Song[]
+): Song[] {
+  return [...firstArray, ...secondArray].sort((a, b) => {
+    const dateA = moment(a.date, "DD.MM.YYYY").unix();
+    const dateB = moment(b.date, "DD.MM.YYYY").unix();
+
+    return dateA - dateB;
+  });
+}
