2ec2b2d610dff6569c084470504c3b676af09d7a
Abdulbaki Dursun <eggsydev@gmail.com>
2021-06-24 17:14:29 +0300

    chore: update file name

--- src/functions/getLocalSongs.ts
+++ src/functions/getLocalSongs.ts
@@ -2,6 +2,6 @@
  * Reads from local asset file and converts content to new metadata style.
  */
 export default function getLocalSongs() {
-  const songs = require("../assets/songs.json");
+  const songs = require("../assets/oldSongs.json");
   return songs;
 }
