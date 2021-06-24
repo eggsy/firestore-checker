8e7d8740ee304392f831c67b98eb60cc48fb2be8
Abdulbaki Dursun <eggsydev@gmail.com>
2021-06-24 17:10:38 +0300

    fix: better way to get video ids

--- src/functions/getVideoId.ts
+++ src/functions/getVideoId.ts
@@ -1,15 +1,19 @@
+/* Import functions */
+import isUrl from "./isUrl";
+
 /**
  * Get YouTube video ID from passed parameter.
  * @param url string
  */
 export default function getVideoId(url: string): string {
-  if (url.includes("youtube.com/watch"))
-    return new URLSearchParams(new URL(url).search).get("v");
-  else if (url.includes("youtu.be/"))
-    return url
-      .split("/")
-      .filter((item) => item)
-      .slice(-1)
-      .join("");
-  else return url;
+  const isParamUrl = isUrl(url);
+
+  if (isParamUrl === true) {
+    const newUrl = new URL(url);
+
+    if (["youtube.com", "music.youtube.com"].includes(newUrl.hostname))
+      return newUrl.searchParams.get("v");
+    else if (newUrl.hostname === "youtu.be")
+      return newUrl.pathname.replace(/\//g, "");
+  } else return url;
 }
