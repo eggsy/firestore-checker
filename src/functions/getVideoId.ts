2e444b5816f73e252c346f89d9744428bbb1f47a
Abdulbaki Dursun <eggsydev@gmail.com>
2021-06-24 16:58:18 +0300

    fix: returns wrong video id when trailing slash

--- src/functions/getVideoId.ts
+++ src/functions/getVideoId.ts
@@ -6,6 +6,10 @@ export default function getVideoId(url: string): string {
   if (url.includes("youtube.com/watch"))
     return new URLSearchParams(new URL(url).search).get("v");
   else if (url.includes("youtu.be/"))
-    return url.split("/")[url.split("/").length - 1];
+    return url
+      .split("/")
+      .filter((item) => item)
+      .slice(-1)
+      .join("");
   else return url;
 }
