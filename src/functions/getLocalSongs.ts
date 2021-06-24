c20f058cc40885432dabefb06633c1bac70c9450
Abdulbaki Dursun <eggsydev@gmail.com>
2021-06-24 16:58:42 +0300

    refactor: no big deal

--- src/functions/getLocalSongs.ts
+++ src/functions/getLocalSongs.ts
@@ -1,18 +1,7 @@
-import moment from "moment";
-
-/* Import functions */
-import getVideoId from "./getVideoId";
-
 /**
  * Reads from local asset file and converts content to new metadata style.
  */
 export default function getLocalSongs() {
   const songs = require("../assets/songs.json");
-
-  return songs.map((song) => {
-    return {
-      date: moment(song.date, "M/DD/YYYY").format("DD.MM.YYYY"),
-      url: getVideoId(song.url),
-    };
-  });
+  return songs;
 }
